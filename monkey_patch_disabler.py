f9819ed7da802068812f271de6387d0b82998d7d
管鐘 <86526364+RogelioKG@users.noreply.github.com>
2023-07-22 18:12:30 +0800

    Update monkey_patch_disabler.py

--- monkey_patch_disabler.py
+++ monkey_patch_disabler.py
@@ -1,3 +1,6 @@
+"""抓猴神器：防止猴子補丁的最佳夥伴"""
+# implemented by metaclass
+
 class MonkeyDisabler(type):
     @classmethod
     def __prepare__(mcs, name, bases, **kwargs):
@@ -6,9 +9,12 @@ class MonkeyDisabler(type):
     def __new__(mcs, name, bases, attrs, **kwargs):
         return super().__new__(mcs, name, bases, attrs)
 
-    def __init__(cls, name, bases, attrs, *, disable):
+    def __init__(cls, name, bases, attrs, *, disable=None):
         super().__init__(name, bases, attrs)
-        cls.__disable = disable
+        if disable is None:
+            cls.__disable = attrs.keys()
+        else:
+            cls.__disable = disable
 
     def __setattr__(cls, key, value):
         if (key == "_MonkeyDisabler__disable") or (key not in cls._MonkeyDisabler__disable):
@@ -16,41 +22,43 @@ class MonkeyDisabler(type):
         else:
             print(f"Monkey patching '{key}' is disabled.")
 
-
+# type of disable: Sequence[str]
 class Cat(metaclass=MonkeyDisabler, disable=["exclaim"]):
+    def __init__(self, name):
+        self.name = name
+        
+    # 若沒有這個 __setattr__，你仍能對個別實例進行猴子補丁
     def __setattr__(self, key, value):
-        print(f"Attempt to set {key}")
         if key not in self.__class__._MonkeyDisabler__disable:
             super().__setattr__(key, value)
         else:
             print(f"Monkey patching '{key}' is disabled.")
 
-    def __init__(self, name):
-        self.name = name
-
     def exclaim(self):
-        print(f"I am wawacat {self.name}!")
+        print(f"{self.name} meow")
 
 
 if __name__ == "__main__":
     cat1 = Cat("Candy")
     cat2 = Cat("Chester")
 
-    print("-------------------------------")
-
     def monkey_exclaim():
         print(r"%%%%%%")
 
     cat1.exclaim()
+    # Candy meow
     cat1.exclaim = monkey_exclaim
+    # Monkey patching 'exclaim' is disabled.
     cat1.exclaim()
-
-    print("-------------------------------")
+    # Candy meow
 
     def monkey_shouting(self):
         print(r"!!!!!!")
 
     cat2.exclaim()
+    # Chester meow
     Cat.exclaim = monkey_shouting
+    # Monkey patching 'exclaim' is disabled.
     cat2.exclaim()
-
+    # Chester meow
+    
